517a7630f4716bd30d8b20a3a004ca4a15e3c87b
Mayen Akpan <neroxma@yahoo.com>
2021-02-10 16:39:39 -0800

    updates

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -9,7 +9,7 @@ authRouter
   .post('/login', jsonBodyParser, (req, res, next) => {
     const { email, password } = req.body
     const loginUser = { email,password }
-
+    console.log(loginUser)
 
     for (const [key, value] of Object.entries(loginUser))
       if (value == null)
